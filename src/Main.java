582caa4feeab4807cece62398b6da5335be01a23
Gabriel Fiuza <34981476+icegabriel@users.noreply.github.com>
2022-04-13 01:15:21 -0300

    Refatorando codigo

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,64 @@
-import espacoDeEstados.*;
-import estrategiasDeBusca.cega.*;
+import espacoDeEstados.Estado;
+import espacoDeEstados.Puzzle8;
+import estrategiasDeBusca.cega.BuscaCega;
+import estrategiasDeBusca.cega.BuscaEmLargura;
+import estrategiasDeBusca.cega.BuscaEmProfundidade;
+import estrategiasDeBusca.cega.BuscaEmProfundidadeLimitada;
 import estrategiasDeBusca.heuristica.AStar;
 import estrategiasDeBusca.heuristica.BestFirst;
-import estrategiasDeBusca.heuristica.BuscaInformada;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	@SuppressWarnings("rawtypes")
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		
-		//char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
-		//char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
-		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
-		char[] cfgFim = {'2','3',' ','7','4','1','5','8','6'};
-		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+		/*
+		char[] cfgIni = {' ','2','3','1','4','6','7','5','8'};
+		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
+		char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+		*/
 
+		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
 		Puzzle8 puzzleInicial = new Puzzle8();
 		puzzleInicial.setEstado(cfgIni);
 		puzzleInicial.setCusto(0);
 		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
-			
+
+		char[] cfgFim = {'2','3',' ','7','4','1','5','8','6'};
 		Puzzle8 puzzleFinal = new Puzzle8();
 		puzzleFinal.setEstado(cfgFim);
 		puzzleFinal.setCusto(0);
 		puzzleFinal.setAvaliacao(0);
 
+		printMenu(puzzleInicial, puzzleFinal);
+	}
+
+	private static void printMenu(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal) {
+
 		Scanner menu = new Scanner(System.in);
 
 		while (true) {
 
-			System.out.print("+-------------------------------------------------+\n");
-			System.out.print("|                     Buscas                      |\n");
-			System.out.print("+-------------------------------------------------+\n");
-			System.out.print("| 1 - Busca em Largura     						|\n");
-			System.out.print("| 2 - Busca em Profundidade          				|\n");
-			System.out.print("| 3 - Busca em Profundidade Limitada          	|\n");
-			System.out.print("| 4 - Busca em Profundidade Limitada Interativa   |\n");
-			System.out.print("| 5 - Busca A*                                    |\n");
-			System.out.print("| 6 - Busca Best First                            |\n");
-			System.out.print("| 7 - Sair              							|\n");
-			System.out.print("+-------------------------------------------------+\n");
-			System.out.print("\n");
+			System.out.println("+-------------------------------------------------+");
+			System.out.println("|                     Buscas                      |");
+			System.out.println("+-------------------------------------------------+");
+			System.out.println("| 1 - Busca em Largura     						  |");
+			System.out.println("| 2 - Busca em Profundidade          			  |");
+			System.out.println("| 3 - Busca em Profundidade Limitada          	  |");
+			System.out.println("| 4 - Busca em Profundidade Limitada Interativa   |");
+			System.out.println("| 5 - Busca A*                                    |");
+			System.out.println("| 6 - Busca Best First                            |");
+			System.out.println("| 7 - Sair              						  |");
+			System.out.println("+-------------------------------------------------+");
+			System.out.println();
 			System.out.print("Digite uma opção: ");
 
 			int opcao = menu.nextInt();
 
-			if (opcao == 7) {
-				break;
-			}
+			if (opcao == 7) break;
 
 			switch (opcao) {
 				case 1:
@@ -88,6 +93,7 @@ public class Main {
 	}
 
 	private static void buscaEmProfundidadeLimitada(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal, int limite) {
+
 		BuscaEmProfundidadeLimitada buscaEmProfundidadeLimitada = new BuscaEmProfundidadeLimitada();
 		buscaEmProfundidadeLimitada.setLimite(limite);
 		buscaEmProfundidadeLimitada.setInicio(puzzleInicial);
@@ -98,6 +104,7 @@ public class Main {
 	}
 
 	private static void buscaEmProfundidade(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal) {
+
 		BuscaCega buscaEmProfundidade = new BuscaEmProfundidade();
 		buscaEmProfundidade.setInicio(puzzleInicial);
 		buscaEmProfundidade.setObjetivo(puzzleFinal);
@@ -106,17 +113,18 @@ public class Main {
 		printCaminho(buscaEmProfundidade.getCaminhoSolucao());
 	}
 
-	private static void buscaEmProfundidadeLimitadaInterativa(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal) throws IOException {
+	private static void buscaEmProfundidadeLimitadaInterativa(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal) {
 
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println();
 		System.out.println("BUSCA EM PROFUNDIDADE LIMITADA INTERATIVA\n");
 
-		System.out.println("Insira o valor Inicial");
-		BufferedReader inputValorInicialBuffer = new BufferedReader(new InputStreamReader(System.in));
-		int valorInicial = Integer.valueOf(inputValorInicialBuffer.readLine());
+		System.out.print("Digite o valor inicial: ");
+		int valorInicial = scanner.nextInt();
 
-		System.out.println("Insira o valor Final");
-		BufferedReader inputValorFinalBuffer = new BufferedReader(new InputStreamReader(System.in));
-		int valorFinal = Integer.valueOf(inputValorFinalBuffer.readLine());
+		System.out.print("Digite o valor final: ");
+		int valorFinal = scanner.nextInt();
 
 		for(int i = valorInicial; i <= valorFinal; i++){
 			buscaEmProfundidadeLimitada(puzzleInicial, puzzleFinal, i);
@@ -124,6 +132,7 @@ public class Main {
 	}
 
 	private static void buscaEmLargura(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal) {
+
 		BuscaCega busca = new BuscaEmLargura();
 		busca.setInicio(puzzleInicial);
 		busca.setObjetivo(puzzleFinal);
@@ -133,6 +142,7 @@ public class Main {
 	}
 
 	private static void buscaAEstrela(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal){
+
 		AStar buscaAEstrela = new AStar();
 		buscaAEstrela.setInicio(puzzleInicial);
 		buscaAEstrela.setObjetivo(puzzleFinal);
@@ -142,6 +152,7 @@ public class Main {
 	}
 
 	private static void buscaBestFirst(Puzzle8 puzzleInicial, Puzzle8 puzzleFinal){
+
 		BestFirst bestFirst = new BestFirst();
 		bestFirst.setInicio(puzzleInicial);
 		bestFirst.setObjetivo(puzzleFinal);
@@ -152,6 +163,7 @@ public class Main {
 	}
 
 	private static void printCaminho(List<Estado<?>> caminhos) {
+
 		for(Estado e : caminhos) {
 			System.out.println(e);
 		}
